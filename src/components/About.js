56b6b588045ede19e02206e01297c725a80ba500
James Burns <jrburns96@aol.com>
2021-11-23 18:41:40 -0500

    commit tree e5554ee054feb62b5427bd4ec2f51ddbe75357f1

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,39 @@
 import React from "react";
 
-function About() {
+function About(props) {
+  console.log(props);
+  function IsBio(){
+    console.log(props.bio)
+    if(props.bio){
+      return (
+        <p>{props.bio}</p>
+      )
+    } else{
+      return null;
+    }
+  }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <IsBio />
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links 
+      github = {props['github']}
+      linkedin = {props['linkedin']}
+      />
     </div>
   );
+}
+
+const Links = (props) => {
+  console.log(props)
+  return (
+  <div>
+  <h3>Links</h3>
+  <a href={props.github}>{props.github}</a>
+  <a href={props.linkedin}>{props.linkedin}</a>
+  </div>
+  )
 }
 
 export default About;
